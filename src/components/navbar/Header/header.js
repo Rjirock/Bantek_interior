747a96661be6197a6099733c7e22776ef33ec1d4
rajwebitya <rjrajirock@gmail.com>
2026-02-13 09:12:12 +0530

    ok masage

--- src/components/navbar/Header/header.js
+++ src/components/navbar/Header/header.js
@@ -59,9 +59,7 @@ export default function Header() {
         <Link href="/" className="flex items-center">
           <div className="relative h-[64px] w-[64px] rounded-full overflow-hidden">
             <Image
-              src="/Logo.png"
-              src="/Logo.png"
-              alt="Bantek logo"
+              src="/Logo.png"              alt="Bantek logo"
               fill
               priority
               className="object-cover scale-[1.18] brightness-110 contrast-110"
