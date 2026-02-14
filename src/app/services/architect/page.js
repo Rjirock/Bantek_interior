de564c6c868c42f061f4da34ddc77631f1d4d3d8
Raj Jaiswal <mensfashionableclub@gmail.com>
2026-02-14 21:16:40 +0530

    Update page.js

--- src/app/services/architect/page.js
+++ src/app/services/architect/page.js
@@ -11,18 +11,3 @@ export default function ExteriorPage() {
       <FourthSection />
     </>
   );
-
-import FirstSection from "@/components/service_compo/exterior/first_section";
-import SecondSection from "../../../components/service_compo/exterior/second_section"
-import ThirdSection from "../../../components/service_compo/exterior/third_section"
-import FourthSection from "../../../components/service_compo/exterior/fourth_section";
-export default function ExteriorPage() {
-  return (
-    <>
-      <FirstSection />
-      <SecondSection />
-      <ThirdSection />
-      <FourthSection />
-    </>
-  );
-}
